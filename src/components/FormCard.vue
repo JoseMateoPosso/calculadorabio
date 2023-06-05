65c2de315f54ecfe1e691ddc0728251cfcfd4e5e
JoseMateoPosso <joseteo@hotmail.es>
2023-06-05 13:12:22 -0500

    Se separó un json de los dialogo de la vista principal + otras mejoras

--- src/components/FormCard.vue
+++ src/components/FormCard.vue
@@ -2,7 +2,7 @@
   <div :class="backgroundClass" id="app">
     <img v-if="!isFirstDialogOpen" :src="headerImage" alt="Imagen de descripción" height="250">
     <v-dialog v-for="(dialog, index) in dialogs" :key="index" v-model="dialog.open" persistent max-width="900">
-      <v-card height="70vh" :class="{ 'fisrt-card': isFirstDialogOpen }">
+      <v-card height="60vh" :class="{ 'fisrt-card': isFirstDialogOpen }">
         <v-icon v-if="index > 0" class="icon-back" @click="changeDialog(index, true)">mdi-arrow-left</v-icon>
         <CardTitle :title="dialog.title" :progress="progressValue" />
         <v-card-text>
@@ -10,14 +10,14 @@
             <v-row>
               <v-col>
                 <p v-html="dialog.content"></p>
-                <img v-if="dialog.img" :src="dialog.img.url" :height="dialog.img.height" />
+                <img v-if="dialog.img" :src="imageMap[dialog.img.url]" :height="dialog.img.height" />
                 <v-text-field variant="solo" v-model="dialog.response.numSelected" type="number"
                   v-if="dialog.requiresResponse && dialog.response.numSelected !== false"></v-text-field>
                 <!--<v-btn v-if="dialog.skipAllowed" @click="changeDialog(index)">No aplica</v-btn>-->
               </v-col>
               <v-col :cols="columnSize" class="text-center" sm="columnSmSize" :class="columnClass">
                 <p v-html="dialog.content2"></p>
-                <img v-if="dialog.img2" :src="dialog.img2.url" :height="dialog.img2.height" />
+                <img v-if="dialog.img2" :src="imageMap[dialog.img2.url]" :height="dialog.img2.height" />
                 <!--<RadioGroupCard v-if="dialog.requiresResponse" :response="dialog.response.radioGroup" />-->
                 <v-radio-group v-if="dialog.requiresResponse" v-model="dialog.response.radioGroup">
                   <v-row class="justify-center">
@@ -25,8 +25,7 @@
                       <v-card @click="dialog.response.radioGroup = item.id"
                         :class="{ 'selected-image': dialog.response.radioGroup === item.id }">
                         <v-radio hide-details :label="item.label" :value="item.id"></v-radio>
-                        <v-img v-if="item.image" height="50" :src="item.image"
-                          :class="{ 'image-filter': dialog.response.radioGroup === item.id }"></v-img>
+                        <img :src="imageMap[item.image]">
                       </v-card>
                     </v-col>
                   </v-row>
@@ -46,6 +45,7 @@
 
 <script>
 import CardTitle from './CardTitle.vue'
+import data from './dialog.json';
 //import RadioGroupCard from './RadioGroupComponent.vue';
 
 export default {
@@ -67,188 +67,26 @@ export default {
           propaneGas: 0
         }
       ],
-      dialogs: [
-        {
-          content2: "En <b>Avgust </b> queremos ayudar al planeta, por eso, te invitamos a reducir tu <b>impacto ambiental</b>. <br><br>Descubre cómo puedes ayudar al planeta, <b>¡Calculando tu huella de carbono ahora!</b>",
-          img: {
-            url: require("@/assets/imgs/texto.png"),
-            height: 180
-          },
-          open: true,
-          requiresResponse: false,
-        },
-        {
-          content: "<b> <h3>¿Qué es la huella de carbono?</h3> </b><br> La huella de carbono representa el volumen total de <b>Gases de Efecto Invernadero (GEI)</b> que producen las actividades económicas y cotidianas del ser humano. <br><br> <b> <h3>¿Para qué se calcula?</h3> </b><br> Es importante entender la huella de carbono como un primer paso en el camino a la mejora y el compromiso de reducción de emisiones de Gases de Efecto Invernadero (GEI) para contribuir a la lucha contra el cambio climático.",
-          open: false,
-          requiresResponse: false,
-          img2: {
-            url: require("@/assets/imgs/ardilla.png"),
-            height: 250
-          },
-        },
-        {
-          content: "<b> <h3>¿Qué necesitas para calcular tu huella?</h3> </b><br> <ul> <li>Necesitas tus facturas de <b>gas y energía eléctrica.</b></li> <li>En tu factura de consumo de <b>gas</b>, te aparece tu consumo en <b>m3</b></li> <li>En tu factura de consumo de <b>energía eléctrica</b>, te aparece tu consumo en <b>kw/h</b> </li> </ul>",
-          open: false,
-          requiresResponse: false,
-          img: {
-            url: require("@/assets/imgs/seccion2.png"),
-            height: 150
-          },
-          img2: {
-            url: require("@/assets/imgs/ardilla.png"),
-            height: 250
-          }
-        },
-        {
-          content: "<h3 class='text-center mb-3'>¿Cuántas personas viven contigo?</h3>",
-          content2: "<h3 class='text-center mb-3'>Zona de Vivienda</h3>",
-          open: false,
-          requiresResponse: true,
-          response: {
-            numSelected: 0,
-            radioGroup: null,
-            items: [
-              { image: require('@/assets/imgs/urbana.png'), label: 'URBANA', id: 'Urbano' },
-              { image: require('@/assets/imgs/rural.png'), label: 'RURAL', id: 'Rural' }
-            ],
-            completionFunction: 'GetPeopleZone' // This function will be called when the dialog is completed
-          },
-        },
-        {
-          content: "<h3 class='text-center mb-3'>¿Cuánto consumes de electricidad al mes? en kWh</h3>",
-          open: false,
-          requiresResponse: true,
-          skipAllowed: true,
-          title: "<h5>Cálculo de vivienda - Zona Rural<h5>",
-          response: {
-            numSelected: 0,
-            electricityConsumption: 0,
-            completionFunction: 'GetEnergyConsumption' // This function will be called when the dialog is completed
-          },
-        },
-        {
-          content: "<h3 class='text-center mb-3'>¿Qué tipo de combustible utilizas para cocinar?</h3>",
-          open: false,
-          requiresResponse: true,
-          title: "<h5>Cálculo de vivienda - Zona Rural<h5>",
-          response: {
-            numSelected: false,
-            items: [
-              { image: require('@/assets/imgs/gaspropano.png'), label: 'GAS PROPANO', id: 'gaspropano' },
-              { image: require('@/assets/imgs/gasnatural.png'), label: 'GAS NATURAL', id: 'gasnatural' },
-            ],
-            completionFunction: 'GetFuelType' // This function will be called when the dialog is completed
-          }
-        },
-        {
-          open: false,
-          requiresResponse: true,
-          title: "<h5>Cálculo de vivienda - Zona Rural<h5>",
-          img2: {
-            url: require("@/assets/imgs/gasnatural.png"),
-            height: 150
-          },
-          content: "¿Cuántos metros cúbicos (m3) aparece en tu recibo de consumo de gas natural al mes? <br><br>",
-          content2: "<h3 class='text-center mb-3'>¿Qué tipo de combustible utilizas para cocinar?</h3> <br> <p>GAS NATURAL</p>",
-          response: {
-            numSelected: 0,
-            completionFunction: 'GetCubicMmeters' // This function will be called when the dialog is completed
-          }
-        },
-        {
-          content: "<h3 class='text-center mb-3'>Consumo de gas propano mensual</h3> <br> <p class='text-center'>• ¿Cuántos cilindros consumes al mes?</p><br>",
-          open: false,
-          requiresResponse: true,
-          title: "<h5>Cálculo de vivienda - Zona Rural<h5>",
-          response: {
-            numSelected: 0,
-            electricityConsumption: 0,
-            completionFunction: 'GetPropaneGas' // This function will be called when the dialog is completed
-          },
-        },
-        {
-          content: "<h3 class='text-center mb-3'>Consumo de gas propano mensual</h3><br>• ¿De que tamaño es tu cilindro de gas?",
-          open: false,
-          requiresResponse: true,
-          title: "<h5>Cálculo de vivienda - Zona Rural<h5>",
-          response: {
-            numSelected: false,
-            items: [
-              { label: '45kg (100lb)', id: '45kg (100lb)' },
-              { label: '15kg (33lb)', id: '15kg (33lb)' },
-              { label: '30kg (77lb)', id: '30kg (77lb' },
-              { label: '18kg (40lb)', id: '18kg (40lb)' },
-              { label: '6 - 4,5kg (10lb)', id: '6 - 4,5kg (10lb)' }
-            ],
-            completionFunction: 'calculateFootprint' // This function will be called when the dialog is completed
-          }
-        },
-        {
-          content: "<h3 class='text-center mb-3'>¿Qué tipo de combustible solido utilizas para cocinar?</h3>",
-          open: false,
-          requiresResponse: true,
-          title: "<h5>Cálculo de vivienda - Zona Rural<h5>",
-          response: {
-            numSelected: false,
-            items: [
-              { image: require('@/assets/imgs/bagazo.png'), label: 'BAGAZO', id: 1 },
-              { image: require('@/assets/imgs/carbon.png'), label: 'CARBÓN', id: 2 },
-              { image: require('@/assets/imgs/leña.png'), label: 'LEÑA', id: 3 },
-              { image: require('@/assets/imgs/madera.png'), label: 'MADERA', id: 4 },
-              { image: require('@/assets/imgs/fibrapalma.png'), label: 'FIBRA PALMA', id: 5 }
-            ],
-            completionFunction: 'calculateFootprint' // This function will be called when the dialog is completed
-          }
-        },
-        {
-          content: "<h3 class='text-center mb-3'>¿Qué tipo de combustible utilizas?</h3>",
-          open: false,
-          requiresResponse: true,
-          title: "<h5>Cálculo de Transporte<h5>",
-          response: {
-            numSelected: false,
-            items: [
-              { image: require('@/assets/imgs/transportemasivo.png'), label: 'SISTEMA DE TRANSPORTE MASIVO', id: 1 },
-              { image: require('@/assets/imgs/vehiculo.png'), label: 'VEHÍCULO PARTICULAR Y/O DE SERVICIO PÚBLICO', id: 2 },
-              { image: require('@/assets/imgs/moto.png'), label: 'MOTO', id: 3 },
-              { image: require('@/assets/imgs/bicicleta.png'), label: 'BICICLETA', id: 4 },
-              { image: require('@/assets/imgs/apie.png'), label: 'A PIE', id: 5 }
-            ],
-            completionFunction: 'calculateFootprint' // This function will be called when the dialog is completed
-          }
-        },
-        {
-          content: "<h3 class='text-center mb-3'>¿Cuántos kilometros recorres al día aproximadamente? en km</h3>",
-          open: false,
-          requiresResponse: true,
-          title: "<h5>Cálculo de Transporte<h5>",
-          response: {
-            numSelected: 0,
-            items: [
-              { image: require('@/assets/imgs/transportemasivo.png'), label: 'SISTEMA DE TRANSPORTE MASIVO', id: 1 },
-              { image: require('@/assets/imgs/vehiculo.png'), label: 'VEHÍCULO PARTICULAR Y/O DE SERVICIO PÚBLICO', id: 2 },
-              { image: require('@/assets/imgs/moto.png'), label: 'MOTO', id: 3 },
-              { image: require('@/assets/imgs/bicicleta.png'), label: 'BICICLETA', id: 4 },
-              { image: require('@/assets/imgs/apie.png'), label: 'A PIE', id: 5 }
-            ],
-            completionFunction: 'calculateFootprint' // This function will be called when the dialog is completed
-          }
-        },
-        {
-          content: "<h3 class='text-center mb-3'>¿Reciclas, Reduces, o Reutilizas?</h3>",
-          open: false,
-          requiresResponse: true,
-          title: "<h5>Manejo de Residuos<h5>",
-          response: {
-            numSelected: false,
-            items: [
-              { label: 'SI', id: 1 },
-              { label: 'NO', id: 2 },
-            ],
-            completionFunction: 'calculateFootprint' // This function will be called when the dialog is completed
-          }
-        }
-      ]
+      dialogs: data,
+      imageMap: {
+        'texto': require('@/assets/imgs/texto.png'),
+        'ardilla': require('@/assets/imgs/ardilla.png'),
+        'seccion2': require('@/assets/imgs/seccion2.png'),
+        'urbana': require('@/assets/imgs/urbana.png'),
+        'rural': require('@/assets/imgs/rural.png'),
+        'gaspropano': require('@/assets/imgs/gaspropano.png'),
+        'gasnatural': require('@/assets/imgs/gasnatural.png'),
+        'bagazo': require('@/assets/imgs/bagazo.png'),
+        'carbon': require('@/assets/imgs/carbon.png'),
+        'leña': require('@/assets/imgs/leña.png'),
+        'madera': require('@/assets/imgs/madera.png'),
+        'fibrapalma': require('@/assets/imgs/fibrapalma.png'),
+        'transportemasivo': require('@/assets/imgs/transportemasivo.png'),
+        'vehiculo': require('@/assets/imgs/vehiculo.png'),
+        'moto': require('@/assets/imgs/moto.png'),
+        'bicicleta': require('@/assets/imgs/bicicleta.png'),
+        'apie': require('@/assets/imgs/apie.png')
+      }
     };
   },
   computed: {
@@ -336,7 +174,7 @@ export default {
       if (numSelected) {
         console.log(numSelected);
       }
-      const imgSrc = require(`@/assets/imgs/${radioGroup}.png`);
+      const imgSrc = radioGroup;
       // Guardar la imagen en CarbonFootPrint
       this.CarbonFootPrint.fuelImage = imgSrc;
       // Si el siguiente diálogo existe, actualiza su imagen.
@@ -350,13 +188,13 @@ export default {
     },
     GetPropaneGas(numSelected) {
       return { propaneGas: numSelected };
-      //console.log("------finalhastaahora----", this.CarbonFootPrint);
     },
     calculateFootprint(numSelected, radioGroup) {
       console.log(numSelected, radioGroup);
+      console.log('------finalhastaahora----', this.CarbonFootPrint);
     },
     calculateFootprintDo(numSelected) {
-      console.log("calculateFootprintDo", numSelected);
+      console.log("rayosientra", numSelected);
     },
   }
 };
