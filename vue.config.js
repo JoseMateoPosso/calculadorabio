18d07765ba6179dbb09a6bbc4d61645726bd1a20
JoseMateoPosso <joseteo@hotmail.es>
2023-10-20 10:30:19 -0500

    Entrega final huella de carbona || ultimas correciones

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'Calcula tu Huella de Carbono - Avgust';
+      return args;
+    });
+  },
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
